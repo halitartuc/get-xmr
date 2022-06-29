46509875d9e2f517cf0a53f6c6ae829c218e0188
Halit Artuç <halitartuc@gmail.com>
2022-06-29 13:16:05 +0300

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -7,7 +7,7 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-focal-x64.tar.gz
-tar xzvf xmrig-6.18.0-focal-x64.tar.gz
-cd xmrig-6.18.0
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
+tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
+cd xmrig-6.16.4
 ./xmrig -o xmrpool.eu:9999 -u 4BHNwbnA8UiFZJMi2qYCyqYTrt8bVrpRx8qrJfwpmq5cLM2mXqtXRZDNTBczym3H2tMULytbsE87ELQminCy8U1YVgDajkz+colab -k --tls
